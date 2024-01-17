9927b759b9ee0f8a99dfa080b2bf0566634d585a
Admin <ninimamatova0403@gmail.com>
2024-01-17 15:27:01 +0500

    All files 3

--- src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
@@ -3,6 +3,6 @@ package school.mjc.stage0.module3.task1;
 public class PrintWordsApp {
     public static void main(String[] args) {
 
-        System.out.println("Java\n"  + "is\n"  + "smth");
+        System.out.println("\nJava"  + "\nis"  + "\nsmth");
     }
 }
